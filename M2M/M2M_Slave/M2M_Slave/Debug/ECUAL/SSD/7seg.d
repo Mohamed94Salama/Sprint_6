e83a8355c364300b3b8af62976ae833e56d82e99
Mohamed94Salama <70778328+Mohamed94Salama@users.noreply.github.com>
2021-09-22 00:15:14 +0200

    Add files via upload

---
+++ M2M/M2M_Slave/M2M_Slave/Debug/ECUAL/SSD/7seg.d
@@ -0,0 +1,71 @@
+ECUAL/SSD/7seg.d ECUAL/SSD/7seg.o: ../ECUAL/SSD/7seg.c \
+ ../ECUAL/SSD/7seg.h ../MCAL/DIO/DIO.h ../MCAL/DIO/DIO_Cfg.h \
+ ../LIB/STD_TYPES.h ../LIB/BIT_MANIPULATE.h \
+ ../Services/FreeRTOS/inc/FreeRTOS.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ ../Services/FreeRTOS/inc/FreeRTOSConfig.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../Services/FreeRTOS/inc/projdefs.h ../Services/FreeRTOS/inc/portable.h \
+ ../Services/FreeRTOS/inc/portmacro.h \
+ ../Services/FreeRTOS/inc/mpu_wrappers.h ../Services/FreeRTOS/inc/task.h \
+ ../Services/FreeRTOS/inc/list.h
+
+../ECUAL/SSD/7seg.h:
+
+../MCAL/DIO/DIO.h:
+
+../MCAL/DIO/DIO_Cfg.h:
+
+../LIB/STD_TYPES.h:
+
+../LIB/BIT_MANIPULATE.h:
+
+../Services/FreeRTOS/inc/FreeRTOS.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+../Services/FreeRTOS/inc/FreeRTOSConfig.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom32.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../Services/FreeRTOS/inc/projdefs.h:
+
+../Services/FreeRTOS/inc/portable.h:
+
+../Services/FreeRTOS/inc/portmacro.h:
+
+../Services/FreeRTOS/inc/mpu_wrappers.h:
+
+../Services/FreeRTOS/inc/task.h:
+
+../Services/FreeRTOS/inc/list.h:
---
+++ M2M/M2M_Slave/M2M_Slave/Debug/Services/CRC16/CRC.d
@@ -0,0 +1,6 @@
+Services/CRC16/CRC.d Services/CRC16/CRC.o: ../Services/CRC16/CRC.c \
+ ../Services/CRC16/CRC.h ../LIB/STD_TYPES.h
+
+../Services/CRC16/CRC.h:
+
+../LIB/STD_TYPES.h:
